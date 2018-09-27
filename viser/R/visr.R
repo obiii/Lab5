2695ac204b2dc1c4137d31ef86f2039f8226af5b
obiii <obaidurrehman1994@gmail.com>
2018-09-27 16:03:27 +0200

    Added getData function

---
+++ viser/R/visr.R
@@ -0,0 +1,39 @@
+
+#' Base Url to get crime data
+#' @export
+baseUrl =  paste0("https://data.montgomerycountymd.gov/resource/yc8a-5df8.json?")
+'%ni%' <- Negate('%in%')
+
+#' Function to clean the data
+cleanData <- function(jsondf){
+  
+  df <- as.data.frame(jsondf)
+  garbCols <- c(colnames(df)[1:12],"end_date","state","start_date","nibrs_code","pra","incident_id","geolocation.type","geolocation.coordinates")
+  #df <- df[,-c(1:12)]
+  df <- df[,colnames(df)%ni%garbCols]
+  
+  df$time = format(as.POSIXct(strptime(df$date,"%Y-%m-%dT%H:%M:%S",tz="")) ,format = "%H:%M")
+  df$date = format(as.POSIXct(strptime(df$date,"%Y-%m-%dT%H:%M:%S",tz="")) ,format = "%Y-%m-%d")
+  
+  return(df)
+}
+
+#' Get data limted by number of observations
+#' @export
+getLimitedData <- function(limit = 5000){
+
+  require(RCurl)
+  require(jsonlite)
+  
+  newUrl = paste0(baseUrl,"$limit=",limit)
+  data <- fromJSON(url)
+  
+  data <- cleanData(data)
+  
+  return(data)
+}
+
+
+
+#x<-getLimitedData(limit=10)
+
