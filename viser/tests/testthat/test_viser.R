5a4f7855ce4fc274a695479d73b71efee41ac692
obiii <obaidurrehman1994@gmail.com>
2018-10-02 15:42:51 +0200

    Added Testcases and vignettes

--- viser/tests/testthat/test_viser.R
+++ viser/tests/testthat/test_viser.R
@@ -5,9 +5,20 @@ x <- getLimitedData(limit = 50)
 test_that("API returns correct number of records", {
   expect_equal(nrow(getLimitedData(limit = 500)), 401)
   expect_equal(nrow(getLimitedData(limit = 10)), 9)
+  expect_equal(ncol(getLimitedData(limit = 10)), 5)
   
 })
 
 test_that("Invalid output.", {
   expect_error(getCrimeByID(x,99))
+})
+
+
+test_that("Invalid data", {
+  expect_error(verify(length(colnames(x))) == 5)
+  expect_error(verify(colnames(x)[1] == "crimeType"))
+  expect_error(verify(colnames(x)[2] == "latitude"))
+  expect_error(verify(colnames(x)[3] == "longitude"))
+  expect_error(verify(colnames(x)[4] == "date"))
+  expect_error(verify(getCrimeByID(2) == "JUVENILE"))
 })
